38680aa6acfcb7665db4d03dbc30346f621d5f10
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2022-12-16 01:19:19 -0300

    Criando menu lateral

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { useSelector } from 'react-redux';
 
+import { Container, Menu, PageBody } from './AppStyleds'
+
 import HomeScreen from './pages/HomeScreen';
 import Tela2Screen from './pages/Tela2Screen';
+
+import MenuItem from './components/MenuItem'
 
 export default () => {
     const name = useSelector(state => state.user.name);
 
     return (
         <BrowserRouter>
-            <h1>Oi, {name}</h1>
+            <Container>
+                <Menu>
+                    <MenuItem icon="/assets/store.png" link="/" />
+                    <MenuItem icon="/assets/order.png" link="/orders" />
+                    <MenuItem icon="/assets/profile.png" link="/profile" />
+                </Menu>
 
-            <Switch>
-                <Route exact path="/">
-                    <HomeScreen />
-                </Route>
-                <Route path="/tela2/:nome">
-                    <Tela2Screen />
-                </Route>
-            </Switch>
+                <PageBody>
+                    <Switch>
+                        <Route exact path="/">
+                            <HomeScreen />
+                        </Route>
+                        <Route path="/tela2/:nome">
+                            <Tela2Screen />
+                        </Route>
+                    </Switch>
+                </PageBody>
+
+            </Container>
         </BrowserRouter>
     );
 }
