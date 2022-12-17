600fd081e43d8792f73787b84fc1415eed8877c6
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2022-12-16 21:19:22 -0300

    Fazendo requisição para buscar todas categorias

--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
 import { Container } from './styled';
+
+import api from '../../api';
 
 import Header from '../../components/Header';
 
 export default () => {
     const [headerSearch, setHeaderSearch] = useState('')
-
     const history = useHistory();
+
+    useEffect(() => {
+        const getCategories = async () => {
+            const categories = await api.getCategories()
+            console.log(categories)
+        }
+
+        getCategories()
+    }, [])
 
     return (
         <Container>
-            <Header search={headerSearch} onSearch={setHeaderSearch}/>
+            <Header search={headerSearch} onSearch={setHeaderSearch} />
         </Container>
     );
 }
